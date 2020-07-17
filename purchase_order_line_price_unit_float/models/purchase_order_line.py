85ad9feaa7e2be40b2ab57a17f124e4b643857aa
victoralmau <victoralmau@gmail.com>
2020-07-17 14:47:26 +0200

    translate

--- purchase_order_line_price_unit_float/models/purchase_order_line.py
+++ purchase_order_line_price_unit_float/models/purchase_order_line.py
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-import logging
-_logger = logging.getLogger(__name__)
 
-from odoo import api, models, fields
+from odoo import models, fields
 import odoo.addons.decimal_precision as dp
+
 
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
