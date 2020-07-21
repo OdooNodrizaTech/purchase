1de63b0050ec0a40fe57ba1f538106db6ab0664b
victoralmau <victoralmau@gmail.com>
2020-07-21 14:48:35 +0200

    fix

--- purchase_order_mail_followers_extra/models/purchase_order.py
+++ purchase_order_mail_followers_extra/models/purchase_order.py
@@ -26,19 +26,23 @@ class PurchaseOrder(models.Model):
                 ('res_model', '=', 'purchase.order'),
                 ('res_id', '=', self.id),
             ])
-            if len(mail_followers_ids) > 0:
+            if mail_followers_ids:
                 for mail_followers_id in mail_followers_ids:
-                    if mail_followers_id.partner_id.id > 0:
+                    if mail_followers_id.partner_id:
                         mail_followers_partner_ids.append(mail_followers_id.partner_id.id)
             
-            purchase_order_mail_followers_extra_ids = self.env['purchase.order.mail.followers.extra'].search([('partner_id', '=', int(self.partner_id.id))])    
+            purchase_order_mail_followers_extra_ids = self.env['purchase.order.mail.followers.extra'].search(
+                [
+                    ('partner_id', '=', int(self.partner_id.id))
+                ]
+            )
             for purchase_order_mail_followers_extra_id in purchase_order_mail_followers_extra_ids:
                 for partner_id_extra in purchase_order_mail_followers_extra_id.partner_ids_extra:
                     if not partner_id_extra.id in mail_followers_partner_ids:   
-                        mail_followers_vals = {
+                        vals = {
                             'partner_id': int(partner_id_extra.id),
                             'res_model': 'purchase.order',
                             'res_id': self.id,
                             'subtype_ids': [(4,1)],                                        
                         }
-                        self.env['mail.followers'].sudo().create(mail_followers_vals)
+                        self.env['mail.followers'].sudo().create(vals)
